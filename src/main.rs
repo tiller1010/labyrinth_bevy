a9fbf6f5609775dc7b40b67592eb6cdd28130bd3
Tyler <tiller1010@gmail.com>
2024-12-22 17:59:46 -0500

    improvements: aligned explanation text

--- src/main.rs
+++ src/main.rs
@@ -59,6 +59,7 @@ fn explain_game(
                 },
             ),
         ])
+        .with_text_justify(JustifyText::Right)
         .with_style(Style {
             position_type: PositionType::Absolute,
             bottom: Val::Px(5.),
