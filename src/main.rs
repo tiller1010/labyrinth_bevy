cf3405a7dc68a427749a759867473f90ff4cd67b
Tyler <tiller1010@gmail.com>
2025-01-11 20:59:19 -0500

    feat: main menu

--- src/main.rs
+++ src/main.rs
@@ -47,6 +47,32 @@ use crate::coins::{
 };
 use music::play_music;
 
+#[derive(Clone, Copy, Default, Eq, PartialEq, Debug, Hash, States)]
+enum MenuState {
+    #[default]
+    Main,
+}
+
+#[derive(Component)]
+struct OnMainMenuScreen;
+
+#[derive(Component)]
+enum MenuButtonAction {
+    Play,
+    Quit,
+}
+
+#[derive(Clone, Copy, Default, Eq, PartialEq, Debug, Hash, States)]
+enum GameState {
+    #[default]
+    Menu,
+    Game,
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+}
+
 fn explain_game(
     commands: &mut Commands,
 ) {
@@ -71,7 +97,93 @@ fn explain_game(
     ));
 }
 
-fn setup(
+fn menu_setup(mut menu_state: ResMut<NextState<MenuState>>) {
+    menu_state.set(MenuState::Main)
+}
+
+fn menu_action(
+    interaction_query: Query<
+        (&Interaction, &MenuButtonAction),
+        (Changed<Interaction>, With<Button>),
+    >,
+    mut app_exit_events: EventWriter<AppExit>,
+    mut game_state: ResMut<NextState<GameState>>,
+) {
+    for (interaction, menu_button_action) in &interaction_query {
+        if *interaction == Interaction::Pressed {
+            match menu_button_action {
+                MenuButtonAction::Quit => {
+                    app_exit_events.send(AppExit::Success);
+                }
+                MenuButtonAction::Play => {
+                    game_state.set(GameState::Game);
+                }
+            }
+        }
+    }
+}
+
+fn main_menu_setup(mut commands: Commands) {
+    let button_style = Style {
+        width: Val::Px(250.),
+        height: Val::Px(65.),
+        margin: UiRect::all(Val::Px(20.)),
+        justify_content: JustifyContent::Center,
+        align_items: AlignItems::Center,
+        ..default()
+    };
+
+    let button_text_style = TextStyle {
+        font_size: 40.,
+        ..default()
+    };
+
+    commands.spawn((
+        NodeBundle {
+            style: Style {
+                width: Val::Percent(100.),
+                height: Val::Percent(100.),
+                align_items: AlignItems::Center,
+                justify_content: JustifyContent::Center,
+                ..default()
+            },
+            ..default()
+        },
+        OnMainMenuScreen,
+    )).with_children(|parent| {
+
+        parent.spawn((
+            ButtonBundle {
+                style: button_style.clone(),
+                ..default()
+            },
+            MenuButtonAction::Play
+        ))
+        .with_children(|parent| {
+            parent.spawn(TextBundle::from_section(
+                "Play",
+                button_text_style.clone(),
+            ));
+        });
+
+        parent.spawn((
+            ButtonBundle {
+                style: button_style.clone(),
+                ..default()
+            },
+            MenuButtonAction::Quit
+        ))
+        .with_children(|parent| {
+            parent.spawn(TextBundle::from_section(
+                "Quit",
+                button_text_style.clone(),
+            ));
+        });
+
+    });
+}
+
+fn game_setup(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
     mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
@@ -86,13 +198,30 @@ fn setup(
     play_music(&mut commands, &asset_server);
 }
 
+fn despawn_screen<T: Component>(to_despawn: Query<Entity, With<T>>, mut commands: Commands) {
+    for entity in &to_despawn {
+        commands.entity(entity).despawn_recursive();
+    }
+}
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .init_state::<MenuState>()
+        .init_state::<GameState>()
         .add_event::<CollisionEvent>()
         .insert_resource(Score(0))
-        .add_systems(Startup, (setup, setup_camera))
-        .add_systems(Update, execute_player_walking_animations)
+        .add_systems(Startup, setup)
+        .add_systems(Startup, setup_camera.run_if(in_state(GameState::Game)))
+        .add_systems(OnEnter(MenuState::Main), main_menu_setup)
+        .add_systems(OnEnter(GameState::Menu), menu_setup)
+        .add_systems(OnExit(GameState::Menu), despawn_screen::<OnMainMenuScreen>)
+        .add_systems(OnEnter(GameState::Game), game_setup)
+        .add_systems(
+            Update,
+            execute_player_walking_animations
+            .run_if(in_state(GameState::Game))
+        )
         .add_systems(
             Update,
             (
@@ -101,6 +230,7 @@ fn main() {
                 trigger_player_walking_animation.run_if(input_pressed(KeyCode::ArrowUp)),
                 trigger_player_walking_animation.run_if(input_pressed(KeyCode::ArrowDown)),
             )
+            .run_if(in_state(GameState::Game))
         )
         .add_systems(
             FixedUpdate,
@@ -118,7 +248,12 @@ fn main() {
                 update_scoreboard,
                 check_for_player_collisions_with_coins,
             )
-            .chain(),
+            .chain()
+            .run_if(in_state(GameState::Game))
+        )
+        .add_systems(
+            Update,
+            menu_action.run_if(in_state(GameState::Menu))
         )
         .run();
 }
